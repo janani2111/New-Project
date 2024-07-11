77c3dc35ef3e10bcb537166535d82dbdc6031b63
janani211 <janijana211108@gmail.com>
2024-07-11 21:28:22 +0530

    First time of New Project

---
+++ src/test/java/org/demo/BookingInformation.java
@@ -0,0 +1,17 @@
+package org.demo;
+
+public class BookingInformation {
+	private void login() {
+		System.out.println("Login Details");
+
+	}
+	private void update() {
+		System.out.println("update Details");
+
+	}
+	private void confirmation() {
+		System.out.println("Login Details");
+
+	}
+
+}
