acbecba676558acaca585c72daafd09a7dc84f14
janani211 <janijana211108@gmail.com>
2024-07-13 20:19:06 +0530

    Added three methods and Added new class with two methods

---
+++ src/test/java/org/demo/Client_Details.java
@@ -0,0 +1,11 @@
+package org.demo;
+
+public class Client_Details {
+	private void clientName() {
+		System.out.println("Janaki");
+	}
+	private void clientPhNo() {
+		System.out.println("7708713369");
+	}
+
+}
